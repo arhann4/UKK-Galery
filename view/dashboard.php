aa5f9fcd1cd9d45270836308f585df465199236d
Virkha <blqsxfynptrie@gmail.com>
2025-01-20 12:33:53 +0700

    p

--- view/dashboard.php
+++ view/dashboard.php
@@ -27,6 +27,8 @@
             <h1 class="moving-text">TRAVEL AND</h1>
             <h1 class="moving-text">ENJOY YOUR</h1>
             <h1 class="moving-text">HOLIDAY</h1>
+            <h1 class="moving-text">HOLIDAY</h1>
+            <h1 class="moving-text">HOLIDAY</h1>
         </div>
    </div>
    <div class="container-fluid container-right">
